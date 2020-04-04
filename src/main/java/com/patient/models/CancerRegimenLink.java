e168b5938de45082924f80b1aa36e564173a06fd
MalliKharjuna <mkrao.malli@gmail.com>
2020-04-03 23:10:45 -0400

    fixed deployment related issues in gcp by adding new default application property

--- src/main/java/com/patient/models/CancerRegimenLink.java
+++ src/main/java/com/patient/models/CancerRegimenLink.java
@@ -6,11 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
